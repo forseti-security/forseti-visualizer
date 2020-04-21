f7e56fe97217cf747ee1167fce83a02c564ec154
garrettwong <garrettwong@google.com>
2020-04-21 15:20:03 -0700

    grpc js test updates

--- forseti-api/grpc.js
+++ forseti-api/grpc.js
@@ -1,54 +1,74 @@
-var PROTO_PATH = '../forseti-api/protos/explain.proto';
-var grpc = require('grpc');
-var protoLoader = require('@grpc/proto-loader');
-// Suggested options for similarity to existing grpc.load behavior
-var packageDefinition = protoLoader.loadSync(
-    PROTO_PATH, {
+/**
+ * Get GRPC Client
+ */
+const grpc = require('grpc')
+const protoLoader = require('@grpc/proto-loader')
+
+// protoPath = 'protos/explain.proto';
+// config = {}
+function getGrpcClient(protoPath, forsetiServerVmIpAddress, config) {
+    config = config || {
         keepCase: true,
         longs: String,
         enums: String,
         defaults: true,
         oneofs: true
-    });
-var protoDescriptor = grpc.loadPackageDefinition(packageDefinition);
-// The protoDescriptor object has the full package hierarchy
-var ex = protoDescriptor.explain;
+    }
 
-let channel = process.env.FORSETI_SERVER_VM_CHANNEL;
+    // Suggested options for similarity to existing grpc.load behavior
+    var packageDefinition = protoLoader.loadSync(protoPath, config)
 
-console.log('channe', channel);
-let res = new ex.Explain(channel, grpc.credentials.createInsecure());
+    var protoDescriptor = grpc.loadPackageDefinition(packageDefinition);
 
-var meta = new grpc.Metadata();
-meta.add('handle', process.env.FORSETI_DATA_MODEL_HANDLE);
+    // The protoDescriptor object has the full package hierarchy
+    let channel = `${forsetiServerVmIpAddress}:50051`; // "35.212.33.44:50051"
+    let explainClient = new protoDescriptor.explain.Explain(channel, grpc.credentials.createInsecure());
+    return explainClient;
+}
 
-// console.log(ex);
-// console.log(res);
-console.log('channel', channel);
-console.log('channel', process.env.FORSETI_DATA_MODEL_HANDLE);
-let iamPrefix='user/garrettwong@gwongcloud.com';
-console.log(iamPrefix);
+function getExplainClientMethods() {
+    var protoPath = 'protos/explain.proto';
+    var forsetiServerVmIpAddress = process.env.FORSETI_SERVER_VM_IP_ADDRESS || '35.232.70.205';
+    var explainClient = getGrpcClient(protoPath, forsetiServerVmIpAddress)
+    // console.log(Object.getOwnPropertyNames(explainClient))
+    console.log(explainClient.constructor.prototype)
+}
 
-// res.getAccessByMembers({
-//     member_name: iamPrefix
-// }, meta, function(first, second) {
-//     console.log('success', first, second);
-// }, function(err) {
-//     console.log('error', err);
-// });
+function getIamExplainResources() {
+    var protoPath = 'protos/explain.proto';
+    var forsetiServerVmIpAddress = process.env.FORSETI_SERVER_VM_IP_ADDRESS || '35.232.70.205';
+    var forsetiModelId = process.env.FORSETI_DATA_MODEL_HANDLE || '1d11c1d62a7e589153f10512d09710fe';
+    var iamPrefix = 'user/garrettwong@gwongcloud.com';
+    var permissions = ['iam.serviceAccounts.actAs'];
 
-// ref: https://grpc.io/docs/tutorials/basic/node/
-//iamPrefix='user/garrettwong@gwongcloud.com';
+    console.log(forsetiServerVmIpAddress, forsetiModelId, iamPrefix)
 
-let call2 = res.getAccessByMembers({
-    member_name: iamPrefix,
-    permission_names: ['iam.serviceAccounts.actAs'],
-    expand_resources: true
-}, meta);
-call2.on('data', function (a, b) {
-    console.log('a:', a);
-    console.log('b:', b);
-})
-call2.on('end', function () {
-    console.log('end');
-});
+    // ref: https://grpc.io/docs/tutorials/basic/node/
+    // iamPrefix='user/garrettwong@gwongcloud.com';
+
+    var explainClient = getGrpcClient(protoPath, forsetiServerVmIpAddress)
+    console.log(explainClient)
+
+    var meta = new grpc.Metadata()
+    meta.add('handle', forsetiModelId)
+
+    let response = explainClient.getAccessByMembers({
+        member_name: iamPrefix,
+        permission_names: permissions,
+        expand_resources: true
+    }, meta)
+
+    var results = []
+    response.on('data', function (res) {
+        console.log('result:', res);
+        results.push(res);
+    })
+
+    response.on('end', function () {
+        console.log('end of stream.  final results:', results);
+    })
+}
+
+// getIamExplainResources();
+
+getExplainClientMethods()
